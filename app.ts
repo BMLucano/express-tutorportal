c627f602ed87ef7e7bad3b0c86dd53ed6b419e74
Brandie Lucano <blucano.dev@gmail.com>
2024-05-01 16:32:00 -0700

    documentation fou auth routes. *some initial set up/imports

--- app.ts
+++ app.ts
@@ -1,14 +1,21 @@
 import express, { Application, Request, Response, NextFunction} from "express";
 import {ExpressError, NotFoundError } from "./expressError.js";
-import { add } from "./add.js";
+import morgan from "morgan";
+import cors from "cors";
+
+//TODO: finish importing routes
+import authRoutes from "./routes/auth"
+// import { add } from "./add.js";
 
 const app: Application = express();
 app.use(express.json());
 
+app.use("/auth", authRoutes);
+
 /** Sample route */
-app.get("/", function (req: Request, res: Response) {
-  return res.send(`2 + 3 = ${add(2, 3)}`);
-});
+// app.get("/", function (req: Request, res: Response) {
+//   return res.send(`2 + 3 = ${add(2, 3)}`);
+// });
 
 /** Handle 404 errors -- this matches everything */
 app.use(function (req: Request, res: Response, next: NextFunction) {
---
+++ routes/auth.ts
@@ -0,0 +1,24 @@
+/** Routes for authentication. */
+import express from "express";
+
+const router = express.Router();
+
+
+/** POST /auth/register: { username, password } => { token }
+ *
+ * Returns JWT token which can be used to authenticate further requests.
+ *
+ * Authorization req: none
+*/
+
+/** POST auth/register: { user } => { token }
+ *
+ * user must include { username, password, firstName, lastName, email, role}
+ *
+ * Returns JWT token which can be used to authenticate further requests.
+ *
+ * Authorization required: none
+ *
+ */
+
+export default router;
