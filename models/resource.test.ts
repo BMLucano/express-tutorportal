dc837b752e5fcef4b1e0e15cef18d587a25c5f33
Brandie Lucano <blucano.dev@gmail.com>
2024-07-11 15:01:09 -0500

    resource model and tests done

---
+++ models/resource.test.ts
@@ -0,0 +1,153 @@
+import { describe, test, it, expect } from "vitest";
+import { NotFoundError, BadRequestError } from "../expressError";
+
+import db from "../db";
+import Resource from "./resource";
+
+import { afterAll, beforeAll } from "vitest";
+import { afterEach, beforeEach } from "node:test";
+import {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
+} from "./_testCommon";
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
+
+/***************** create */
+
+describe("create", function () {
+  const newData = {
+    studentUsername: "u1",
+    title: "Resource1",
+    url: "www.example.com",
+    description: "This is a resource",
+  };
+
+  test("works", async function () {
+    let resource = await Resource.create(newData);
+    expect(resource).toEqual({
+      id: expect.any(Number),
+      ...newData,
+    });
+
+    const result = await db.query(
+      `SELECT student_username, title, url, description
+       FROM resources
+       WHERE student_username = 'u1' AND title = 'Resource1' AND url = 'www.example.com' AND description = 'This is a resource'`
+    );
+    expect(result.rows).toEqual([
+      {
+        student_username: "u1",
+        title: "Resource1",
+        url: "www.example.com",
+        description: "This is a resource",
+      },
+    ]);
+  });
+
+  test("bad request with dupe", async function () {
+    try {
+      await Resource.create(newData);
+      await Resource.create(newData);
+      throw new Error("fail test, you shouldn't get here");
+    } catch (err) {
+      expect(err instanceof BadRequestError).toBeTruthy();
+    }
+  });
+});
+
+
+/************ update */
+
+describe("update", function () {
+  const updateData = {
+    title: "New Title",
+    url: "www.newexample.com",
+  };
+
+  test("works", async function () {
+    let resource = await Resource.update(1, updateData);
+    expect(resource).toEqual({
+      id: 1,
+      studentUsername: "u1",
+      ...updateData,
+      description: "This is a resource",
+    });
+
+    const result = await db.query(
+      `SELECT student_username, title, url, description
+       FROM resources
+       WHERE id = 1`
+    );
+    expect(result.rows).toEqual([
+      {
+        student_username: "u1",
+        title: "New Title",
+        url: "www.newexample.com",
+        description: "This is a resource",
+      },
+    ]);
+  });
+
+  test("not found", async function () {
+    try {
+      await Resource.update(999, updateData);
+      throw new Error("fail test, you shouldn't get here");
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+});
+
+
+/****************** delete */
+
+describe("delete", function () {
+  test("works", async function () {
+    await Resource.delete(1);
+    const result = await db.query(`SELECT * FROM resources WHERE id = 1`);
+    expect(result.rows).toEqual([]);
+  });
+
+  test("not found", async function () {
+    try {
+      await Resource.delete(999);
+      throw new Error("fail test, you shouldn't get here");
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+});
+
+
+/**************** getResourcesByStudent */
+
+describe("getResourcesByStudent", function () {
+  test("works", async function () {
+    let resources = await Resource.getResourcesByStudent("u1");
+    expect(resources).toEqual([
+      {
+        id: 1,
+        studentUsername: "u1",
+        title: "Resource1",
+        url: "www.example.com",
+        description: "This is a resource",
+      },
+    ]);
+  });
+
+  test("not found", async function () {
+    try {
+      await Resource.getResourcesByStudent("u3");
+      throw new Error("fail test, you shouldn't get here");
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+});
---
+++ models/resource.ts
@@ -0,0 +1,69 @@
+/**
+ * Resource model for creating, updating, deleting, and retrieving resources.
+ */
+
+class Resource {
+
+  /**
+   * Creates a new resource and updates db.
+   * @param {ResourceDataToCreate} data - The resource data.
+   * @returns {ResourceData} - the created resource
+   * @throws {BadRequestError} - if resource already in db.
+   */
+  static async create(data: ResourceDataToCreate): Promise<ResourceData>{}
+
+
+  /**
+   * Updates a resource in db with partial data or all data.
+   *
+   * @param {ResourceDataToUpdate} data - Partial or full resource data to update.
+   * @param {number} id - Resource id to update
+   * @returns {ResourceData} - Updated resource
+   * @throws {NotFoundError} - if resource not found
+   */
+  static async update(id: number, data: ResourceDataToUpdate): Promise<ResourceData>{}
+
+
+  /**
+   * Deletes a resource from db by id.
+   *
+   * @param {number} id - Resource id to delete
+   * @returns {undefined}
+   * @throws {NotFoundError} - if resource not found
+   */
+  static async delete(id: number): Promise<void>{}
+
+
+  /**
+   * Gets resources based on student.
+   *
+   * @param {string} studentUsername
+   * @returns {ResourceData[]} - a list of all resources for a student
+   * @throws {NotFoundError} - if student not found
+   */
+  static async getResourcesByStudent(studentUsername: string): Promise<ResourceData[]>{}
+}
+
+type ResourceDataToUpdate = {
+  studentUsername?: string | null,
+  title?: string | null,
+  url?: string | null,
+  description?: string | null,
+}
+
+type ResourceDataToCreate = {
+  studentUsername: string,
+  title: string,
+  url: string,
+  description?: string,
+}
+
+type ResourceData = {
+  id: number,
+  studentUsername: string,
+  title: string,
+  url: string,
+  description: string
+}
+
+export default Resource;
