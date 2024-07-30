06bba263dc62893453e5b72d0e1a46fc4c15cd3a
Brandie Lucano <blucano.dev@gmail.com>
2024-07-30 15:16:11 -0500

    testCommon setup

---
+++ routes/_testCommon.ts
@@ -0,0 +1,11 @@
+import db from "../db";
+import User from "../models/user";
+import Assignment from "../models/assignment";
+import AssignmentStudent from "../models/assignmentStudent";
+import Question from "../models/question";
+import Submission from "../models/submission";
+import Note from "../models/note";
+import Resource from "../models/resource";
+import Session from "../models/session";
+
+import { createToken } from "../models/helpers/tokens";
